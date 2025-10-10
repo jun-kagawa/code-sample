1aca6bcc5f9b4fafa058ccb7b5c4cea0aad06f92
heyjun3 <t3k2g5kako@yahoo.co.jp>
2025-10-11 08:49:35 +0900

    update

---
+++ leet/108-convert-sorted-array-to-binary-search-tree/main.rb
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode
+    attr_accessor :val, :left, :right
+    def initialize(val = 0, left = nil, right = nil)
+        @val = val
+        @left = left
+        @right = right
+    end
+end
+# @param {Integer[]} nums
+# @return {TreeNode}
+def sorted_array_to_bst(nums)
+  return nil if nums.nil? || nums.empty?
+  mid = nums.size / 2
+  root = TreeNode.new(nums[mid])
+  root.left = sorted_array_to_bst(nums[0...mid])
+  root.right = sorted_array_to_bst(nums[mid+1..-1])
+  root
+end
